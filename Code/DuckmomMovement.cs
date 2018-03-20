f2d9272774dae2ebbfbe487fb7a8e85bb16b5062
Paav0 <37539187+Paav0@users.noreply.github.com>
2018-03-20 10:29:35 +0200

    Add files via upload

---
+++ Code/DuckmomMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckmomMovement : MonoBehaviour
+{
+    private float fTurnRate = 200.0f;
+    private float fSpeed = 5.0f;
+
+
+
+    
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.forward * fTurnRate * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(-Vector3.forward * fTurnRate * Time.deltaTime);
+        transform.localPosition = transform.localPosition + transform.up * fSpeed *Time.deltaTime;
+
+   
+
+    }
+}
