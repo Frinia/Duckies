1998142cd32578c9bf2003fa0ea368f0e2ade05d
Frinia <37539182+Frinia@users.noreply.github.com>
2018-03-26 12:14:09 +0300

    Created Explosion.cs and updated Destroyer.cs

---
+++ Destroyer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    public Explosion exp;
+    /*exp = ducklingPrefab.GetComponent<Explosion>();
+    exp.Explode();*/
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        GameObject g = col.gameObject;
+        Destroy(col.gameObject);
+        exp = gameObject.GetComponent<Explosion>();
+        exp.Explode(g);
+    }
+}
---
+++ Explosion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    public GameObject pooffab;
+
+    public void Explode(GameObject g)
+    {
+        GameObject expl = Instantiate(pooffab, g.transform.position, Quaternion.identity);
+        Destroy(expl, 1.3f);
+    }
+}
